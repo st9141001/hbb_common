a7c98fd500ceca1ceece2e59fa027cb1558ef446
st9141001 <st9141001@gmail.com>
2026-07-24 18:22:15 +0800

    tako: bake self-hosted rendezvous server + public key

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["tako-st9141001.duckdns.org"];
+pub const RS_PUB_KEY: &str = "DN38crbAkdO6YTtmtKpNJmRpPx4ZWIFRxjXo2hXRqY0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
